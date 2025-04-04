fa4bba5cb6bd77733dbb50f62d8502160de9c2b6
3omarQ <145579400+3omarQ@users.noreply.github.com>
2025-04-04 17:03:30 +0100

    Change api call endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,7 +63,7 @@ function App() {
     emptyClassificationResults();
     
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://quote-classifier-coft.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
